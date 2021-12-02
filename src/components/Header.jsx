fae5a750106666ca5bece4388ccd0aa90908de00
johnbabu021 <johnbabu021@gmail.com>
2021-12-02 20:51:02 +0530

    material icons installation

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../styles/Header.css'
+import '../../public/SWEETGRADIENTS.svg'
+import GitHubIcon from '@mui/icons-material/GitHub';
+export default function Header(){
+    return(
+        <div    className="header">
+<img src="SWEETGRADIENTS.svg"  alt="" />
+
+        </div>
+    )
+}
