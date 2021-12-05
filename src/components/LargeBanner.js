fe0df9f2b7b6b87e06b001b5fec9f7a10bbd4f4d
johnbabu021 <johnbabu021@gmail.com>
2021-12-05 21:47:19 +0530

    rotation enabled

--- src/components/LargeBanner.js
+++ src/components/LargeBanner.js
@@ -5,13 +5,43 @@ import CodeIcon from '@mui/icons-material/Code';
 import AddIcon from '@mui/icons-material/Add';
 import DownloadIcon from '@mui/icons-material/Download';
 import DocTitle from '../hooks/DocTitle';
+import { useState } from 'react';
 
 const   LargeBanner=({setBanner,item})=>{
+    const   [rotate,setRotate]=useState('to left')
+    const   [length,setLength]=useState(1)
+    const   gradientPath=[
+        {location:'to left'},
+        {location:'to right'},
+        {location:'to top'},
+        {location:'to bottom'},
+      
+      
+    ]
 const leftIcons=[
-    {Icon:RotateRightIcon},
-    {Icon:CodeIcon},
-    {Icon:AddIcon},
-    {Icon:DownloadIcon}
+    {Icon:RotateRightIcon,onClick:()=>
+        {
+            if(length===gradientPath.length-1){
+                setRotate(gradientPath[0].location)
+                setLength(1)
+               
+            }
+            
+
+            else{
+            setLength(length+1)
+
+                setRotate(gradientPath[length].location)
+
+            }
+         
+            
+        },
+        id:1
+    },
+    {Icon:CodeIcon,id:2},
+    {Icon:AddIcon,id:3},
+    {Icon:DownloadIcon,id:4}
 ]
     return(
        <div className="gradient__header">
@@ -20,12 +50,12 @@ const leftIcons=[
 <div    className="grad__name">{item.colors.join('→')}</div>
 
 <div    className="right__icons">
-{leftIcons.map(({Icon})=>(<Icon></Icon>))}
+{leftIcons.map(({Icon,onClick,id})=>(<Icon key={id}  onClick={onClick}></Icon>))}
 </div>
 
 </div>
 
- <div    className="full__gradient" style={{backgroundImage:`linear-gradient(to right,${item.colors})`}}>
+ <div    className="full__gradient" style={{backgroundImage:`linear-gradient(${rotate},${item.colors})`}}>
 {item.name}
 
         </div>
